6c6a2d6dd5596ef0c8ba895b2fd44319cf31f570
Spencer Murphy <murphysw@rose-hulman.edu>
2013-03-20 14:44:47 -0400

    created a parser that takes in a file and converts it into a panel

--- Civilization/src/Panel.java
+++ Civilization/src/Panel.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Panel {
 
 	private Tile[][] tiles;
@@ -29,4 +35,41 @@ public class Panel {
 		this.isExplored = !this.isExplored;
 	}
 
+	public void readFromFile(File file) {
+		BufferedReader reader = null;
+
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String text = null;
+
+			int index = 0;
+			while ((text = reader.readLine()) != null) {
+				String[] stringTiles;
+				stringTiles = text.split("_");
+
+				System.out.println(String.format("i = %d, j = %d",
+						(int) (Math.floor(index / 4)), (index % 4)));
+				this.tiles[(int) (Math.floor(index / 4))][(index % 4)] = new Tile(
+						stringTiles[0], Integer.parseInt(stringTiles[1]),
+						Integer.parseInt(stringTiles[2]), stringTiles[3],
+						Integer.parseInt(stringTiles[4]), stringTiles[5],
+						Integer.parseInt(stringTiles[6]));
+
+				System.out.println(text);
+				System.out.println("  ");
+				index++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
 }
