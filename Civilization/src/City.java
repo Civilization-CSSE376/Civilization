bdc7e635e053ddc0e5f6f93fd318f204a171312c
Josh <harbisjs@rose-hulman.edu>
2013-03-23 14:30:36 -0400

    finish getOutskirts. working on validOutskirts

--- Civilization/src/City.java
+++ Civilization/src/City.java
@@ -53,82 +53,213 @@ public class City {
 		return 0;
 	}
 	
-	private HashSet<Tile> getOutskirts(Tile startTile){ //move to City probably
+	private HashSet<Tile> getOutskirts(Tile startTile){
 		Panel startPanel = Board.findPanel(startTile);
+		
 		Hashtable<String, Panel> neighbors = startPanel.getNeighbors();
+		HashSet<Tile> outskirts = new HashSet<Tile>();
+		
+		Panel westPanel = neighbors.get("West");
+		Panel eastPanel = neighbors.get("East");
+		Panel southPanel = neighbors.get("South");
+		Panel northPanel = neighbors.get("North");
+
 		int startX = startTile.getxPos();
 		int startY = startTile.getyPos();
-		HashSet<Tile> outskirts = new HashSet<Tile>();
+		
 		
 		if(startX - 1 < 0 && startY - 1 < 0){
 			//bottom left corner
-		}
-		
-		if(startX + 2 > startPanel.getTiles().length && startY - 1 < 0){
+			
+			Panel southWestPanel = neighbors.get("South").getNeighbors().get("West");
+			
+			if(southPanel == null || westPanel == null || southWestPanel == null){
+				return null;
+			}
+			
+			//west panel
+			outskirts.add(westPanel.getTiles()[westPanel.getTiles().length - 1][startY]);
+			outskirts.add(westPanel.getTiles()[westPanel.getTiles().length - 1][startY + 1]);
+			
+			//south panel
+			outskirts.add(southPanel.getTiles()[startX][southPanel.getTiles()[0].length - 1]);
+			outskirts.add(southPanel.getTiles()[startX + 1][southPanel.getTiles()[0].length - 1]);
+			
+			//southWest panel
+			outskirts.add(southWestPanel.getTiles()
+					[southWestPanel.getTiles().length][southWestPanel.getTiles().length]);
+			
+			//start panel
+			outskirts.add(startPanel.getTiles()[startX + 1][startY]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX][startY + 1]);
+			
+			
+		}else if(startX + 2 > startPanel.getTiles().length && startY - 1 < 0){
 			//bottom right corner
-		}
-		
-		if(startX - 1 < 0 && startY + 2 > startPanel.getTiles()[0].length){
+			
+			Panel southEastPanel = neighbors.get("South").getNeighbors().get("East");
+			
+			if(southPanel == null || eastPanel == null || southEastPanel == null){
+				return null;
+			}
+			
+			//east panel
+			outskirts.add(eastPanel.getTiles()[0][startY]);
+			outskirts.add(eastPanel.getTiles()[0][startY + 1]);
+			
+			//south panel
+			outskirts.add(southPanel.getTiles()[startX - 1][southPanel.getTiles()[0].length - 1]);
+			outskirts.add(southPanel.getTiles()[startX][southPanel.getTiles()[0].length - 1]);
+			
+			//southEast panel
+			outskirts.add(southEastPanel.getTiles()[0][southEastPanel.getTiles().length]);
+			
+			//start panel
+			outskirts.add(startPanel.getTiles()[startX - 1][startY]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX][startY + 1]);
+		}else if(startX - 1 < 0 && startY + 2 > startPanel.getTiles()[0].length){
 			//top left corner
-		}
-		
-		if(startX + 2 > startPanel.getTiles().length && startY + 2 > startPanel.getTiles()[0].length){
+			
+			Panel northWestPanel = neighbors.get("North").getNeighbors().get("West");
+			
+			if(northPanel == null || westPanel == null || northWestPanel == null){
+				return null;
+			}
+			
+			//west panel
+			outskirts.add(westPanel.getTiles()[westPanel.getTiles().length - 1][startY - 1]);
+			outskirts.add(westPanel.getTiles()[westPanel.getTiles().length - 1][startY]);
+			
+			//north panel	
+			outskirts.add(northPanel.getTiles()[startX][0]);
+			outskirts.add(northPanel.getTiles()[startX + 1][0]);
+			
+			//northWest panel
+			outskirts.add(northWestPanel.getTiles()[northWestPanel.getTiles().length][0]);
+			
+			//start panel
+			outskirts.add(startPanel.getTiles()[startX][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY]);
+		}else if(startX + 2 > startPanel.getTiles().length && startY + 2 > startPanel.getTiles()[0].length){
 			//top right corner
-		}
-		
-		if(startX - 1 < 0){
-			//need west panel
-			Panel westPanel = neighbors.get("West");
+			
+			Panel northEastPanel = neighbors.get("North").getNeighbors().get("East");
+			
+			if(northPanel == null || eastPanel == null || northEastPanel == null){
+				return null;
+			}
+			
+			//east panel
+			outskirts.add(eastPanel.getTiles()[0][startY - 1]);
+			outskirts.add(eastPanel.getTiles()[0][startY]);
+			
+			//north panel	
+			outskirts.add(northPanel.getTiles()[startX - 1][0]);
+			outskirts.add(northPanel.getTiles()[startX][0]);
+			
+			//northEast panel
+			outskirts.add(northEastPanel.getTiles()[0][0]);
+			
+			//start panel
+			outskirts.add(startPanel.getTiles()[startX - 1][startY]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX][startY - 1]);
+		}else if(startX - 1 < 0){
+			//west edge
+			
+			if(westPanel == null){
+				return null;
+			}
+			
+			//west panel
 			outskirts.add(westPanel.getTiles()[westPanel.getTiles().length - 1][startY - 1]);
 			outskirts.add(westPanel.getTiles()[westPanel.getTiles().length - 1][startY]);
 			outskirts.add(westPanel.getTiles()[westPanel.getTiles().length - 1][startY + 1]);
 			
-		}else{
-			outskirts.add(startPanel.getTiles()[startX - 1][startY - 1]);
-			outskirts.add(startPanel.getTiles()[startX - 1][startY]);
-			outskirts.add(startPanel.getTiles()[startX - 1][startY + 1]);
-		}
-		if(startX + 2 > startPanel.getTiles().length){ //tricky boundaries with .length CHECK
-			//need east panel
-			Panel eastPanel = neighbors.get("East");
+			//start panel
+			outskirts.add(startPanel.getTiles()[startX][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX][startY - 1]);
+			
+		}else if(startX + 2 > startPanel.getTiles().length){
+			//east edge
+			
+			if(eastPanel == null){
+				return null;
+			}
+			
+			//east panel
 			outskirts.add(eastPanel.getTiles()[0][startY - 1]);
 			outskirts.add(eastPanel.getTiles()[0][startY]);
 			outskirts.add(eastPanel.getTiles()[0][startY + 1]);
 			
-		}else{
-			outskirts.add(startPanel.getTiles()[startX + 1][startY - 1]);
-			outskirts.add(startPanel.getTiles()[startX + 1][startY]);
-			outskirts.add(startPanel.getTiles()[startX + 1][startY + 1]);
-		}
-		if(startY - 1 < 0){
-			//need south panel
-			Panel southPanel = neighbors.get("South");
+			//start panel
+			outskirts.add(startPanel.getTiles()[startX][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX][startY - 1]);
+			
+		}else if(startY - 1 < 0){
+			//south edge
+			
+			if(southPanel == null){
+				return null;
+			}
+			
+			//south panel
 			outskirts.add(southPanel.getTiles()[startX - 1][southPanel.getTiles()[0].length - 1]);
 			outskirts.add(southPanel.getTiles()[startX][southPanel.getTiles()[0].length - 1]);
 			outskirts.add(southPanel.getTiles()[startX + 1][southPanel.getTiles()[0].length - 1]);
+			
+			//start panel
+			outskirts.add(startPanel.getTiles()[startX - 1][startY]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY]);
 				
-		}else{
-			outskirts.add(startPanel.getTiles()[startX - 1][startY - 1]);
-			outskirts.add(startPanel.getTiles()[startX][startY - 1]);
-			outskirts.add(startPanel.getTiles()[startX + 1][startY - 1]);
-		}
-		if(startY + 2 > startPanel.getTiles()[0].length){ //tricky boundaries with .length CHECK
-			//need north panel	
-			Panel northPanel = neighbors.get("North");
+		}else if(startY + 2 > startPanel.getTiles()[0].length){
+			//north edge
+			
+			if(northPanel == null){
+				return null;
+			}
+			
+			//north panel	
 			outskirts.add(northPanel.getTiles()[startX - 1][0]);
 			outskirts.add(northPanel.getTiles()[startX][0]);
 			outskirts.add(northPanel.getTiles()[startX + 1][0]);
 			
+			//start panel
+			outskirts.add(startPanel.getTiles()[startX - 1][startY]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY]);
+			
 		}else{
+			//start panel
 			outskirts.add(startPanel.getTiles()[startX - 1][startY + 1]);
 			outskirts.add(startPanel.getTiles()[startX][startY + 1]);
 			outskirts.add(startPanel.getTiles()[startX + 1][startY + 1]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY]);
+			outskirts.add(startPanel.getTiles()[startX + 1][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY - 1]);
+			outskirts.add(startPanel.getTiles()[startX - 1][startY]);
 		}
 		
-		return null;
+		return outskirts;
 	}
 	
 	private boolean validOutskirts(HashSet<Tile> outskirts){
+		
 		return false;
 	}
 	
