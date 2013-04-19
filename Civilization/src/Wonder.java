af7105ce520ce186358dc5c96008b96b883e2766
Josh <harbisjs@rose-hulman.edu>
2013-04-18 22:15:09 -0400

    added wonder

---
+++ Civilization/src/Wonder.java
@@ -0,0 +1,78 @@
+
+public class Wonder extends Marker {
+	
+	public int tier = 0;
+
+	public Wonder(String name) {
+		super(name);
+		this.createWonder(name);
+	}
+	
+	private void createWonder(String name){
+		switch(name){
+		case "Stonehenge":
+			this.culture = 1;
+			this.tier = 1;
+			this.cost = 10;
+			break;
+		case "TheOracle":
+			this.culture = 1;
+			this.tier = 1;
+			this.cost = 15; //10 w/Code of Laws
+			break;
+		case "TheHangingGardens":
+			this.culture = 1;
+			this.tier = 1;
+			this.cost = 15; //10 w/Animal Husbandry
+			break;
+		case "TheColossus":
+			this.culture = 1;
+			this.tier = 1;
+			this.cost = 15; //10 w/Metalworking
+			break;
+		case "TheLouvre":
+			this.culture = 2;
+			this.tier = 2;
+			this.cost = 20; //15 w/Printing Press
+			break;
+		case "AngkorWat":
+			this.culture = 2;
+			this.tier = 2;
+			this.cost = 20; //10 w/Theology
+			break;
+		case "HimejiSamuraiCastle":
+			this.culture = 2;
+			this.tier = 2;
+			this.cost = 20; //15 w/Monarchy
+			break;
+		case "PorcelainTower":
+			this.culture = 2;
+			this.tier = 2;
+			this.cost = 20; //15 w/Construction
+			break;
+		case "StatueOfLiberty":
+			this.culture = 3;
+			this.tier = 3;
+			this.cost = 25; //20 w/Metal Casting
+			break;
+		case "SydneyOperaHouse":
+			this.culture = 3;
+			this.tier = 3;
+			this.cost = 25;
+			break;
+		case "UnitedNations":
+			this.culture = 3;
+			this.tier = 3;
+			this.cost = 20;
+			break;
+		case "PanamaCanal":
+			this.culture = 3;
+			this.tier = 3;
+			this.cost = 25; //20 w/Engineering
+			break;
+		default:
+			break;
+		}
+	}
+
+}
